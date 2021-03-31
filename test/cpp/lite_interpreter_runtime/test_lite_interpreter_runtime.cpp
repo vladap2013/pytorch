498fd554549358573299a246ab573d58576cec1a
Martin Yuan <iseeyuan@yahoo.com>
2021-03-30 23:11:02 -0700

    [PyTorch] Lite interpreter with a backend delegate
    Unclean files during sync - Sat Mar 20 04:00:02 PDT 2021
    
    Unclean files during sync - Sun Mar 21 04:00:01 PDT 2021
    
    Differential Revision: [D27232309](https://our.internmc.facebook.com/intern/diff/D27232309/)
    
    ghstack-source-id: a7483ad029be37edd291d46f916aa8ccf5d8f35b
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/54462

--- test/cpp/lite_interpreter_runtime/test_lite_interpreter_runtime.cpp
+++ test/cpp/lite_interpreter_runtime/test_lite_interpreter_runtime.cpp
@@ -49,6 +49,25 @@ TEST(RunTimeTest, LoadAndForward) {
   ASSERT_EQ(result, expected_result);
 }
 
+TEST(RunTimeTest, Delegate) {
+  std::string filePath(__FILE__);
+  auto testModelFile = filePath.substr(0, filePath.find_last_of("/\\") + 1);
+  // The mobile model delegated to the "backend_with_compiler_demo" backend
+  // The model is from the jit code:
+  //  Module m("m");
+  //  m.define(R"(
+  //    def forward(self, x, h):
+  //        return x + h
+  //  )");
+  testModelFile.append("delegate_test.ptl");
+  auto mlm = _load_for_mobile(testModelFile);
+  std::vector<IValue> inputs;
+  inputs.emplace_back(2.0 * at::ones({}));
+  inputs.emplace_back(1.0 * at::ones({}));
+
+  auto mres = mlm.forward(inputs);
+  AT_ASSERT(mres.toTensor().equal(3 * at::ones({})));
+}
 } // namespace mobile
 } // namespace jit
 } // namespace torch
